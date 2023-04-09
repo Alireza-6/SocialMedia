ae2d539abc4a5b29fb772a47704bd82abd938cf7
Alireza-6 <areza.mirzayi@gmail.com>
2023-04-09 16:25:17 +0330

    post and subscription table added

--- blog/apis/urls.py
+++ blog/apis/urls.py
@@ -1,6 +1,3 @@
 from django.urls import path
-from blog.apis.products import ProductApi
 
-urlpatterns = [
-    path('product', ProductApi.as_view(), name='product')
-]
+urlpatterns = []
--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,30 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 from core.models import BaseModel
+from users.models import BaseUser
 
 
-class Product(BaseModel):
-    name = models.TextField(max_length=255)
+class Post(BaseModel):
+    slug = models.SlugField(primary_key=True, max_length=100)
+    title = models.CharField(max_length=100, unique=True)
+    content = models.CharField(max_length=1000)
+    author = models.ForeignKey(BaseUser, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.slug
+
+
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(BaseUser, on_delete=models.CASCADE, related_name="subs")
+    target = models.ForeignKey(BaseUser, on_delete=models.CASCADE, related_name="targets")
+
+    class Meta:
+        unique_together = ("subscriber", "target")
+
+    def clean(self):
+        if self.subscriber == self.target:
+            raise ValidationError({"subscriber": "Subscriber Cannot Be Equal To Target"})
+
+    def __str__(self):
+        return f"{self.subscriber.email} - {self.target.email}"
